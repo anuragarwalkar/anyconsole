190af3a4269d7a86d8b9cfc41cb8e01f409be241
Anurag Arwalkar <anuragarwalkar@gmail.com>
2021-03-29 19:29:52 +0530

    added keymaping

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,54 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+
+type supportedLang = "ts" | "js" | "dart";
+
+const getLogStatementByLang = (lang: supportedLang, textVar: any) => {
+  let statement = "";
+
+  switch (lang) {
+    case "js":
+    case "ts": {
+      //   if (str.indexOf('\'') >= 0 && str.indexOf('"') >= 0) {
+      //     //do something
+      //  }
+      statement = `console.log('${textVar}',${textVar});`;
+    }
+  }
+
+  return statement;
+};
 
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerTextEditorCommand(
+    "anyconsole.log",
+    () => {
+      const editor = vscode.window.activeTextEditor;
 
+      if (!editor) {
+        return;
+      }
 
-	let disposable = vscode.commands.registerCommand('anyconsole.helloWorld', () => {
+      let sel = editor.selection;
+      let doc = editor.document;
+      const [ext] = doc.fileName.split(".").splice(-1);
 
-		vscode.window.showInformationMessage('Hello World from anyconsole!');
-	});
+      if (ext) {
+        let ran = new vscode.Range(sel.start, sel.end);
 
-	context.subscriptions.push(disposable);
+        const text = editor.document.getText(editor.selection);
+
+        editor.edit((edit) => {
+          edit.replace(
+            editor.selection,
+            getLogStatementByLang(ext as supportedLang, text)
+          );
+        });
+      }
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
